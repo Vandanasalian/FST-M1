bd586eb026c16eb021962a36e004e50b5accc359
Vandana <102787864+Vandanasalian@users.noreply.github.com>
2022-05-12 18:46:29 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name =input("Enter your name:")
+age =int( input("Enter your age:"))
+x=str((2022-age)+100)
+print("After 100 years you will be:"+x+" years old")
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+x = int( input("Enter a number:"))
+num = x%2
+if num==0:
+    print("Even Number")
+else:
+    print("Odd Number")
