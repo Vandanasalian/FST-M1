974385357899e15882d0c3d87e5ffa01de333556
Vandana <102787864+Vandanasalian@users.noreply.github.com>
2022-05-13 16:54:05 +0530

    Add files via upload

---
+++ Appium/Activities/Activity3.java
@@ -0,0 +1,99 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+public class Activity3 {
+    // Declare Android driver
+    AndroidDriver<MobileElement> driver;
+    WebDriverWait wait;
+    @BeforeClass
+    public void setup() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "ff111fc");
+        caps.setCapability("platformName", "android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.chrome");
+        caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        caps.setCapability("noReset", true);
+
+        // Instantiate Appium Driver
+        driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void add() {
+        driver.findElementById("digit_5").click();
+        driver.findElementById("op_add").click();
+        driver.findElementById("digit_9").click();
+        // Perform Calculation
+        driver.findElementById("eq").click();
+
+        // Display Result
+        String result = driver.findElementById("result").getText();
+        System.out.println(result);
+        Assert.assertEquals(result, "14");
+    }
+
+    @Test
+    public void subtract() {
+        driver.findElementById("digit_1").click();
+        driver.findElementById("digit_0").click();
+        driver.findElementById("op_sub").click();
+        driver.findElementById("digit_5").click();
+        // Perform Calculation
+        driver.findElementById("eq").click();
+
+        // Display Result
+        String result = driver.findElementById("result").getText();
+        System.out.println(result);
+        Assert.assertEquals(result, "5");
+    }
+
+    @Test
+    public void multiply() {
+        driver.findElementById("digit_5").click();
+        driver.findElementById("op_mul").click();
+        driver.findElementById("digit_1").click();
+        driver.findElementById("digit_0").click();
+        driver.findElementById("digit_0").click();
+        // Perform Calculation
+        driver.findElementById("eq").click();
+
+        // Display Result
+        String result = driver.findElementById("result").getText();
+        System.out.println(result);
+        Assert.assertEquals(result, "500");
+    }
+
+    @Test
+    public void divide() {
+        driver.findElementById("digit_5").click();
+        driver.findElementById("digit_0").click();
+        driver.findElementById("op_div").click();
+        driver.findElementById("digit_2").click();
+        // Perform Calculation
+        driver.findElementById("eq").click();
+
+        // Display Result
+        String result = driver.findElementById("result").getText();
+        System.out.println(result);
+        Assert.assertEquals(result, "25");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
